df64110d43671013fc6227339862e9dd37716455
mritunjayRoy <mroy8202@gmail.com>
2024-12-16 19:16:51 +0530

    user registration model, controller, route added

--- server/app.js
+++ server/app.js
@@ -5,13 +5,20 @@ const cors = require('cors');
 const app = express();
 const database = require('./config/database');
 
+const userRoutes = require('./routes/user.routes')
+
 // connect to database
 database.connect();
 
+// middlewares
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.use('/api/v1/user', userRoutes);
 
 module.exports = app;
