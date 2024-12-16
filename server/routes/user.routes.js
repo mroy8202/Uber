ee9e99ce10ee1b63e8ba7c5a78bd0daa8b8a24a6
mritunjayRoy <mroy8202@gmail.com>
2024-12-16 19:24:08 +0530

    user login functionality added

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { body } = require("express-validator")
 const userController = require('../controllers/user.controller');
 
-
+// register route
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
@@ -11,5 +11,14 @@ router.post('/register', [
 ],
     userController.registerUser
 )
+
+// login route
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    userController.loginUser
+)
+
 
 module.exports = router;
